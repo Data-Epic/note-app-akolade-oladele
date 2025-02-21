b56efef22018bc8ac6395cabf551685eb176f078
Akolade Oladele <aoladele52@gmail.com>
2025-02-21 20:39:48 +0100

    created base note class and sub classes

---
+++ Note_app.py
@@ -0,0 +1,9 @@
+"""creating a base class"""
+class Note:
+"""Initialiing the class"""
+    def __init__(self, content, created_at)
+        self.content = pass
+        self.created_at = pass
+"""creating the display() method"""
+    def display(self)
+        print()
